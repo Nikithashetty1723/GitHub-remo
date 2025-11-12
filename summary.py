d4e9f68fc15e2c68ad2bc2622804105da92dd7d4
EC2 Default User <ec2-user@ip-172-31-75-179.ec2.internal>
2025-11-12 08:54:38 +0000

    adding all the summary syntax in python

---
+++ summary.py
@@ -0,0 +1,11 @@
+| Category           | Examples                           |
+| ------------------ | ---------------------------------- |
+| **Basic Syntax**   | Indentation, comments              |
+| **Data Types**     | int, float, str, list, tuple, dict |
+| **Control Flow**   | if, for, while                     |
+| **Functions**      | def, return, lambda                |
+| **OOP**            | class, self, init                  |
+| **Modules**        | import, from                       |
+| **Error Handling** | try, except                        |
+| **File I/O**       | open(), read(), write()            |
+
