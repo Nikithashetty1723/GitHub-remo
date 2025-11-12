ed9983618a7fc139ac41cf71b7cc20194f9227da
EC2 Default User <ec2-user@ip-172-31-75-179.ec2.internal>
2025-11-12 08:45:56 +0000

    Adding the bulit-in commands

--- keywords.py
+++ keywords.py
@@ -10,3 +10,15 @@
 | `pass`    | `raise`    | `return`   | `try`    |
 | `while`   | `with`     | `yield`    |          |
 
+
+These are Bulit-in Commands:
+    | Category                   | Examples                                                                      |
+| -------------------------- | ----------------------------------------------------------------------------- |
+| **Input/Output**           | `print()`, `input()`                                                          |
+| **Type Conversion**        | `int()`, `float()`, `str()`, `bool()`, `list()`, `tuple()`, `dict()`, `set()` |
+| **Mathematical**           | `abs()`, `pow()`, `round()`, `min()`, `max()`, `sum()`                        |
+| **Iteration / Sequence**   | `len()`, `range()`, `enumerate()`, `zip()`, `sorted()`, `reversed()`          |
+| **Object / Type Handling** | `type()`, `id()`, `dir()`, `isinstance()`, `issubclass()`                     |
+| **Functional Programming** | `map()`, `filter()`, `reduce()` *(from functools)*, `any()`, `all()`          |
+| **Utility / Misc.**        | `help()`, `eval()`, `exec()`, `hash()`, `vars()`, `globals()`, `locals()`     |
+
